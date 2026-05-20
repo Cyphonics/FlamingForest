cdea4661809a12c3eb3ea9f2baee7528737bae70
Cyphonics <JonathonBWard@hotmail.com>
2026-05-20 11:42:39 +0100

    Added timer and fixed movement

--- FlamingForest-Unity/Assets/Scripts/PlayerMovement.cs
+++ FlamingForest-Unity/Assets/Scripts/PlayerMovement.cs
@@ -18,23 +18,23 @@ public class PlayerMovement : MonoBehaviour
 
     public void MoveLeft()
     {
-        // go to the Rigidbody2D stored in our variable and change the linear velocty's X value to -1 (left)
+        // go to the Rigidbody2D stored in our variable and change the linear velocty's X value to -speed (left)
         physicsBody.linearVelocityX = -speed;
     }
 
     public void MoveRight()
     {
-        // go to the Rigidbody2D stored in our variable and change the linear velocty's X value to -1 (left)
+        // go to the Rigidbody2D stored in our variable and change the linear velocty's X value to speed (right)
         physicsBody.linearVelocityX = speed;
     }
     public void MoveUp()
     {
-        // go to the Rigidbody2D stored in our variable and change the linear velocty's X value to -1 (left)
-        physicsBody.linearVelocityY = speed;
+        // go to the Rigidbody2D stored in our variable and change the linear velocty's X value to speed (up)
+        physicsBody.linearVelocityY= speed;
     }
     public void MoveDown()
     {
-        // go to the Rigidbody2D stored in our variable and change the linear velocty's X value to -1 (left)
+        // go to the Rigidbody2D stored in our variable and change the linear velocty's X value to -speed (down)
         physicsBody.linearVelocityY = -speed;
     }
 
